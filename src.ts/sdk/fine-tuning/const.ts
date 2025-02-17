3f5f23efa18f64606dabd129d5711778ec9151d4
Sidonie <sidonie@0g.ai>
2025-02-17 11:35:50 +0000

    Feat(ft): add model for cocktailsdg-opt1.3b

--- src.ts/sdk/fine-tuning/const.ts
+++ src.ts/sdk/fine-tuning/const.ts
@@ -44,6 +44,11 @@ export const MODEL_HASH_MAP: {
         description:
             'DeepSeek-R1-Zero, a model trained via large-scale reinforcement learning (RL) without supervised fine-tuning (SFT) as a preliminary step, demonstrated remarkable performance on reasoning. More details can be found at: https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B',
     },
+    'cocktailsgd-opt-1.3b': {
+        turbo: '0xe25963fd25fe37d7df5216de1eae533ea42090d3642c3f84edd0f179ffc63a94,0xfccaf17bd0ed26b74e8a3883f5c814bcb5f247015d68fd65a28bf98e1bdb0b7f',
+        standard: '',
+        description: 'CocktailSGD-opt-1.3B finetunes the Opt-1.3B langauge model with CocktailSGD, which is a novel distributed finetuning framework. More details can be found at: https://github.com/DS3Lab/CocktailSGD'
+    },
     // TODO: remove
     'mock-model': {
         turbo: '0xf463fe8c26e7dbca20716eb3c81ac1f3ea23a6c5dbe002bf46507db403c71578',
