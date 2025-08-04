e48c8c86a49587650cd1bd26cccb341f03277012
Ravenyjh <jiahaoyan20172@gmail.com>
2025-08-04 20:28:16 +0800

    feat: Add source maps and refactor imports for dynamic loading in Node.js environment

---
+++ lib.esm/index-66ce4df7.js
@@ -0,0 +1,8 @@
+export { d as download, u as upload } from './index-06ca78ab.js';
+import 'ethers';
+import 'crypto-js';
+import 'circomlibjs';
+import 'child_process';
+import 'path';
+import 'fs/promises';
+//# sourceMappingURL=index-66ce4df7.js.map
