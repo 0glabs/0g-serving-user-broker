e36cf204a38086e64f2d6f9a1dc54898d5560b6f
Ravenyjh <jiahaoyan20172@gmail.com>
2025-08-09 00:28:24 +0800

    feat: update contract addresses in ZGComputeNetworkBroker

--- src.ts/sdk/broker.ts
+++ src.ts/sdk/broker.ts
@@ -38,9 +38,9 @@ export class ZGComputeNetworkBroker {
  */
 export async function createZGComputeNetworkBroker(
     signer: JsonRpcSigner | Wallet,
-    ledgerCA = '0x1a85Dd32da10c170F4f138d082DDc496ab3E5BAa',
-    inferenceCA = '0x5299bd255B76305ae08d7F95B270A485c6b95D54',
-    fineTuningCA = '0xda478Ccf5d534346A16b1475E4c2DecE0268B176',
+    ledgerCA = '0x20f6E41b27fB6437B6ED61a42DcddB6328749F84',
+    inferenceCA = '0x9A30Ae15ee33Bbd777113c9C64b31d7f717C97A0',
+    fineTuningCA = '0x4c871c46D901990987EBf39Cb8a16fC73390a4E5',
     gasPrice?: number,
     maxGasPrice?: number,
     step?: number
